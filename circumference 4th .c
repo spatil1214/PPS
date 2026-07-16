270c609ef040a5179049b73aa9b5897d7cb1a8bc
Samiksha Pandav <samikshapandav75@gmail.com>
2026-07-16 23:14:25 +0530

    Add files via upload

---
+++ circumference 4th .c
@@ -0,0 +1,20 @@
+//name: samiksha pandav 
+//prn:2503033111372L012
+
+#include<stdio.h>
+
+int main() {
+    int number;
+    
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    
+    if(number % 2 == 0) {
+        printf("Number is Even\n");
+    } 
+    else {
+        printf("Number is Odd\n");
+    } // This closes the else block
+    
+    return 0;
+} // This closes the main function

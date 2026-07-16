c5bea35355097ede50afc8a4428186146d69c16f
Samiksha Pandav <samikshapandav75@gmail.com>
2026-07-16 23:15:51 +0530

    Add files via upload

---
+++ factorial using recursion (1).c
@@ -0,0 +1,28 @@
+//name: samiksha pandav 
+//prn:2503033111372L012
+
+#include <stdio.h>
+
+int fact(int n); // Function declaration
+
+int main() {
+    int n, ans;
+    
+    printf("Enter number:\t");
+    scanf("%d", &n);
+    
+    ans = fact(n); // Function calling
+    
+    printf("Answer is %d", ans);
+    return 0;
+}
+
+// Function definition
+int fact(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } 
+    else {
+        return n * fact(n - 1);
+    }
+}

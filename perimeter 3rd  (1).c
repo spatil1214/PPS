66e8dac3d244b85de10caaa79e68c1f88ee65b5e
Samiksha Pandav <samikshapandav75@gmail.com>
2026-07-16 23:13:34 +0530

    Add files via upload

---
+++ perimeter 3rd  (1).c
@@ -0,0 +1,27 @@
+//name: samiksha pandav 
+//prn:2503033111372L012 
+#include <stdio.h>
+int main(){
+	int width,breadth,perimeter;
+	
+	
+	printf("Enter width:\t");
+	
+	
+	scanf("%d",&width);
+	
+	
+	printf("Enter breadth:\t");
+	
+	
+	scanf("%d",&breadth);
+	
+	
+	perimeter=(2*width+2*breadth) ;
+	
+	
+	printf("perimeter of Rectangle is %d",perimeter);
+	
+	
+	return 0;
+}

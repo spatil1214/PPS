ad86005e87bfaddd30d6fd3bae447cf1ff28031f
Samiksha Pandav <samikshapandav75@gmail.com>
2026-07-16 23:16:31 +0530

    Add files via upload

---
+++ Additional of two Array .c
@@ -0,0 +1,23 @@
+//name: Samiksha pandav 
+//prn:2503033111372L012
+
+
+#include<stdio.h>
+int main(){
+    int a[5]={11,12,13,14,15};
+    int b[5]={91,81,71,61,15};
+    int c[5];
+    int i;
+    
+    for(i=0; i<=4; i++){
+        c[i]=a[i]+b[i];
+    }
+    
+    printf("Addition of two array is : ");
+    for(i=0;i<=4;i++){
+        printf(" %d\t",c[i]);
+    }
+    
+    return 0;
+}
+

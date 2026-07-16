04a417ed36f35da371e4a3ab250def91326547ff
Samiksha Pandav <samikshapandav75@gmail.com>
2026-07-16 23:16:10 +0530

    Add files via upload

---
+++ pattern printing (1).c
@@ -0,0 +1,17 @@
+//name:  samiksha pandav 
+//prn:2503033111372L012
+
+#include<stdio.h>
+int main(){
+    int i,j,k;
+    for(i=1;i<=3;i++){
+        for(k=1;k<i;k++){
+            printf("\t");
+        }
+        for(j=i;j<=3;j++){
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
